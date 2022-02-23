a5569a5b88f01a5276313c179d2cb8902e9ea895
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-23 12:25:11 +0530

    Optional Parameter in TS

--- 9.optionsParameter.ts
+++ 9.optionsParameter.ts
@@ -8,3 +8,15 @@ function area(a:number,b?:number){
 
 console.log("area rect 10x12 ", area(10,12))
 console.log("Area of square side 4 =",area(4))
+
+/*
+
+Another way to handle the above problem.\
+Giving the default value to the 2nd argument.
+// Now also we can call the function without the 2nd argument
+function area(a:number,b:number=a){
+	
+	return a * b
+}
+
+*/
