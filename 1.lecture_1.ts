98e9b012b0955fd1a71ec8720e20b89b73f3d658
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-23 10:13:21 +0530

    Datatypes in Typescript

---
+++ 1.lecture_1.ts
@@ -0,0 +1,11 @@
+let x = 1;
+// x = "asd"; -> Error we cannot assign "asd" to type 'number'.
+x = 23;
+
+
+// Now "y" can only hold string values. At the time of declaration only
+// We have made it String only variable. Statically Typed.
+
+// Pascal Notation. We put the datatype After the variable name.
+let y:string; // Typescript Notation for defining the datatype of a variable.
+
