e89bd43bdc08eeb950a8b0a222238264c4c7eedb
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-27 12:09:21 +0530

    Alias in TS

---
+++ 19.Alias.ts
@@ -0,0 +1,7 @@
+
+// We can Use Type Alias for increasing reusability of a specific type.
+
+type varType = string | number | undefined
+let a: varType = 20
+let b: varType = "string"
+let c: varType = undefined
